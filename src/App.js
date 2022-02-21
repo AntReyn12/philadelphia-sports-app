b87460bd998bac697ea1ae8e784261f03cff1aad
ant_reyn <antreyn@udel.edu>
2022-02-21 16:57:54 -0500

    created the home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import eaglesLogo from "./img/eagles-logo.png";
+import philliesLogo from "./img/phillies-logo.png";
+import flyersLogo from "./img/flyers-logo.png";
+import sixersLogo from "./img/sixers-logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Philadelphia Sports Rosters</header>
+      <div className="home">
+        <div className="home-intro">
+          <h1 className="home-h1">
+            As a die-hard Philadelphia sports fan, there's few things in this world I love more than my favorite teams. But it can be hard to keep track of every player on the roster, especially after
+            an active off-season. That's where my website can help.
+          </h1>
+          <h1 className="home-h1">
+            The goal of this website is to provide up-to-date rosters for the Eagles, Phillies, Flyers and 76ers. Below you can find all the information you need about who is on the team, their
+            position, number and photo. I hope you enjoy. Go Philly.
+          </h1>
+        </div>
+        <div class="teams">
+          <img src={eaglesLogo} alt="Eagles Logo" />
+          <img src={philliesLogo} alt="Phillies Logo" />
+          <img src={flyersLogo} alt="Flyers Logo" />
+          <img src={sixersLogo} alt="Sixers Logo" />
+        </div>
+      </div>
     </div>
   );
 }
