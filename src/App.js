dce1d8e64bd106afea51b0b2e744b58b97656d76
ant_reyn <antreyn@udel.edu>
2022-02-21 17:10:12 -0500

    minor styling changes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import flyersLogo from "./img/flyers-logo.png";
 import sixersLogo from "./img/sixers-logo.png";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header>Philadelphia Sports Rosters</header>
       <div className="home">
         <div className="home-intro">
-          <h1 className="home-h1">
+          <h1 className="home-h1 left">
             As a die-hard Philadelphia sports fan, there's few things in this world I love more than my favorite teams. But it can be hard to keep track of every player on the roster, especially after
             an active off-season. That's where my website can help.
           </h1>
-          <h1 className="home-h1">
+          <h1 className="home-h1 right">
             The goal of this website is to provide up-to-date rosters for the Eagles, Phillies, Flyers and 76ers. Below you can find all the information you need about who is on the team, their
             position, number and photo. I hope you enjoy. Go Philly.
           </h1>
@@ -29,6 +29,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
