eae87d55911dd7ebf7ad5d03b64d533df67367a8
ant_reyn <antreyn@udel.edu>
2022-02-24 20:48:50 -0500

    finished the eagles roster component

--- src/Components/EaglesRoster.jsx
+++ src/Components/EaglesRoster.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import eagles from "../Rosters/Eagles";
 
 const EaglesRoster = () => {
+  const [position, setPosition] = useState("All");
+  const filteredPositions = eagles.filter((currentPosition) => {
+    if (position === "All") {
+      return currentPosition;
+    } else {
+      return currentPosition.playerPosition === position;
+    }
+  });
+  const handleFilter = (e) => {
+    setPosition(e.target.value);
+  };
+
   return (
-    <div className="roster">
-      <h1>Player name</h1>
-      <p>Player picture</p>
-      <p>Player number</p>
-      <p>Player position</p>
+    <div className="eaglesRoster">
+      <div className="filter">
+        <label>Filter by position</label>
+        <select value={position} onChange={handleFilter}>
+          <option value="All">All</option>
+          <option value="QB">QB</option>
+          <option value="RB">RB</option>
+          <option value="WR">WR</option>
+          <option value="TE">TE</option>
+          <option value="C">C</option>
+          <option value="G">G</option>
+          <option value="T">T</option>
+          <option value="DE">DE</option>
+          <option value="DT">DT</option>
+          <option value="LB">LB</option>
+          <option value="CB">CB</option>
+          <option value="S">S</option>
+          <option value="K">K</option>
+          <option value="P">P</option>
+          <option value="LS">LS</option>
+        </select>
+      </div>
+      {filteredPositions.map((eagle) => {
+        const {playerName, playerNumber, playerPosition} = eagle;
+        return (
+          <div className="eaglesPlayer" key={playerNumber}>
+            <h1 className="playerName">{playerName}</h1>
+            <p className="roster-p">#{playerNumber}</p>
+            <p className="roster-p">{playerPosition}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/Rosters/Eagles.js
@@ -0,0 +1,374 @@
+const eagles = [
+  {
+    playerName: "Jalen Hurts",
+    playerNumber: 1,
+    playerPosition: "QB",
+  },
+  {
+    playerName: "Gardner Minshew",
+    playerNumber: 10,
+    playerPosition: "QB",
+  },
+  {
+    playerName: "Reid Sinnett",
+    playerNumber: 7,
+    playerPosition: "QB",
+  },
+  {
+    playerName: "Miles Sanders",
+    playerNumber: 26,
+    playerPosition: "RB",
+  },
+  {
+    playerName: "Boston Scott",
+    playerNumber: 35,
+    playerPosition: "RB",
+  },
+  {
+    playerName: "Jordan Howard",
+    playerNumber: 24,
+    playerPosition: "RB",
+  },
+  {
+    playerName: "Kenneth Gainwell",
+    playerNumber: 14,
+    playerPosition: "RB",
+  },
+  {
+    playerName: "Jason Huntley",
+    playerNumber: 32,
+    playerPosition: "RB",
+  },
+  {
+    playerName: "Devonta Smith",
+    playerNumber: 6,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "Quez Watkins",
+    playerNumber: 16,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "Jalen Reagor",
+    playerNumber: 18,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "J.J. Arcega-Whiteside",
+    playerNumber: 19,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "John Hightower",
+    playerNumber: 82,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "Greg Ward",
+    playerNumber: 84,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "Deon Cain",
+    playerNumber: 85,
+    playerPosition: "WR",
+  },
+  {
+    playerName: "Dallas Goedert",
+    playerNumber: 88,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Tyree Jackson",
+    playerNumber: 80,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Richard Rodgers",
+    playerNumber: 81,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Jack Stoll",
+    playerNumber: 89,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Jason Croom",
+    playerNumber: 81.5,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Noah Togiai",
+    playerNumber: 83,
+    playerPosition: "TE",
+  },
+  {
+    playerName: "Jordan Mailata",
+    playerNumber: 68,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Lane Johnson",
+    playerNumber: 65,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Andre Dillard",
+    playerNumber: 77,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Le'Raven Clark",
+    playerNumber: 74,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Jack Driscoll",
+    playerNumber: 63,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Brett Toth",
+    playerNumber: 64,
+    playerPosition: "T",
+  },
+  {
+    playerName: "Landon Dickerson",
+    playerNumber: 69,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Nate Herbig",
+    playerNumber: 67,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Sua Opeta",
+    playerNumber: 78,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Isaac Seumalo",
+    playerNumber: 56,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Kayode Awosika",
+    playerNumber: 72,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Jason Kelce",
+    playerNumber: 62,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Jack Anderson",
+    playerNumber: 76,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Derek Barnett",
+    playerNumber: 96,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Brandon Graham",
+    playerNumber: 55,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Tarron Jackson",
+    playerNumber: 75,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Ryan Kerrigan",
+    playerNumber: 90,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Matt Leo",
+    playerNumber: 66,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Cameron Malveaux",
+    playerNumber: 59,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Josh Sweat",
+    playerNumber: 94,
+    playerPosition: "DE",
+  },
+  {
+    playerName: "Fletcher Cox",
+    playerNumber: 91,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Javon Hargrave",
+    playerNumber: 97,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Hassan Ridgeway",
+    playerNumber: 98,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Marlon Tuipulotu",
+    playerNumber: 95,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Milton Williams",
+    playerNumber: 93,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Marvin Wilson",
+    playerNumber: 73,
+    playerPosition: "DT",
+  },
+  {
+    playerName: "Genard Avery",
+    playerNumber: 58,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Shaun Bradley",
+    playerNumber: 54,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "T.J. Edwards",
+    playerNumber: 57,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Christian Elliss",
+    playerNumber: 53,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Patrick Johnson",
+    playerNumber: 48,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Joe Ostman",
+    playerNumber: 0,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Alex Singleton",
+    playerNumber: 49,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "JaCoby Stevens",
+    playerNumber: 30,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Davion Taylor",
+    playerNumber: 52,
+    playerPosition: "LB",
+  },
+  {
+    playerName: "Tay Gowan",
+    playerNumber: 36,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Craig James",
+    playerNumber: 31,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Avonte Maddox",
+    playerNumber: 29,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Mac McCain III",
+    playerNumber: 37,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Zech McPhearson",
+    playerNumber: 27,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Steven Nelson",
+    playerNumber: 3,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Josiah Scott",
+    playerNumber: 33,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Darius Slay",
+    playerNumber: 2,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Andre Chachere",
+    playerNumber: 21,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Kary Vincent Jr.",
+    playerNumber: 34,
+    playerPosition: "CB",
+  },
+  {
+    playerName: "Marcus Epps",
+    playerNumber: 22,
+    playerPosition: "S",
+  },
+  {
+    playerName: "Anthony Harris",
+    playerNumber: 28,
+    playerPosition: "S",
+  },
+  {
+    playerName: "Jared Mayden",
+    playerNumber: 41,
+    playerPosition: "S",
+  },
+  {
+    playerName: "Rodney McLeod",
+    playerNumber: 23,
+    playerPosition: "S",
+  },
+  {
+    playerName: "K'Von Wallace",
+    playerNumber: 42,
+    playerPosition: "S",
+  },
+  {
+    playerName: "Jake Elliott",
+    playerNumber: 4,
+    playerPosition: "K",
+  },
+  {
+    playerName: "Arryn Siposs",
+    playerNumber: 8,
+    playerPosition: "P",
+  },
+  {
+    playerName: "Rick Lovato",
+    playerNumber: 45,
+    playerPosition: "LS",
+  },
+];
+
+export default eagles;
