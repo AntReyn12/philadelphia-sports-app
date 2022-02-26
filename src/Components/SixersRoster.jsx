33c52518f1538cf98165f6b98b9854584695d7a1
ant_reyn <antreyn@udel.edu>
2022-02-26 17:31:43 -0500

    finished the sixers roster

--- src/Components/SixersRoster.jsx
+++ src/Components/SixersRoster.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import sixers from "../Rosters/Sixers";
 
 const SixersRoster = () => {
+  const [position, setPosition] = useState("All");
+  const filteredPositions = sixers.filter((currentPosition) => {
+    if (position === "All") {
+      return currentPosition;
+    } else {
+      return currentPosition.playerPosition === position;
+    }
+  });
+
+  const handleFilter = (e) => {
+    setPosition(e.target.value);
+  };
   return (
-    <div className="roster">
-      <h1>Player name</h1>
-      <p>Player picture</p>
-      <p>Player number</p>
-      <p>Player position</p>
+    <div className="sixersRoster">
+      <div className="sixers-filter">
+        <label>Filter by position</label>
+        <select value={position} onChange={handleFilter}>
+          <option value="All">All</option>
+          <option value="PG">PG</option>
+          <option value="SG">SG</option>
+          <option value="SF">SF</option>
+          <option value="PF">PF</option>
+          <option value="C">C</option>
+        </select>
+      </div>
+      {filteredPositions.map((sixer) => {
+        const {playerName, playerNumber, playerPosition} = sixer;
+        return (
+          <div className="sixersPlayer" key={playerNumber}>
+            <h1 className="playerName">{playerName}</h1>
+            <p className="roster-p">#{playerNumber}</p>
+            <p className="roster-p">{playerPosition}</p>
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 };
---
+++ src/Rosters/Sixers.js
@@ -0,0 +1,94 @@
+const sixers = [
+  {
+    playerName: "James Harden",
+    playerNumber: 1,
+    playerPosition: "PG",
+  },
+  {
+    playerName: "Tyrese Maxey",
+    playerNumber: 0,
+    playerPosition: "PG",
+  },
+  {
+    playerName: "Myles Powell",
+    playerNumber: 5,
+    playerPosition: "PG",
+  },
+  {
+    playerName: "Charlie Brown",
+    playerNumber: 16,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Isaiah Joe",
+    playerNumber: 7,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Braxton Key",
+    playerNumber: 2,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Furkan Korkmaz",
+    playerNumber: 30,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Shake Milton",
+    playerNumber: 18,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Jaden Springer",
+    playerNumber: 11,
+    playerPosition: "SG",
+  },
+  {
+    playerName: "Danny Green",
+    playerNumber: 14,
+    playerPosition: "SF",
+  },
+  {
+    playerName: "Paul Reed",
+    playerNumber: 44,
+    playerPosition: "SF",
+  },
+  {
+    playerName: "Matisse Thybulle",
+    playerNumber: 22,
+    playerPosition: "SF",
+  },
+  {
+    playerName: "Tobias Harris",
+    playerNumber: 12,
+    playerPosition: "PF",
+  },
+  {
+    playerName: "Paul Millsap",
+    playerNumber: 8,
+    playerPosition: "PF",
+  },
+  {
+    playerName: "Georges Niang",
+    playerNumber: 20,
+    playerPosition: "PF",
+  },
+  {
+    playerName: "Charles Bassey",
+    playerNumber: 23,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Willie Cauley-Stein",
+    playerNumber: "00",
+    playerPosition: "C",
+  },
+  {
+    playerName: "Joel Embiid",
+    playerNumber: 21,
+    playerPosition: "C",
+  },
+];
+
+export default sixers;
