da383b36334fa39ec516a17a265d90a6f35b08fa
ant_reyn <antreyn@udel.edu>
2022-02-26 15:04:36 -0500

    finished the flyers roster

--- src/Components/FlyersRoster.jsx
+++ src/Components/FlyersRoster.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import flyers from "../Rosters/Flyers";
 
 const FlyersRoster = () => {
+  const [position, setPosition] = useState("All");
+  const filteredPositions = flyers.filter((currentPosition) => {
+    if (position === "All") {
+      return currentPosition;
+    } else {
+      return currentPosition.playerPosition === position;
+    }
+  });
+  const handleFilter = (e) => {
+    setPosition(e.target.value);
+  };
   return (
-    <div className="roster">
-      <h1>Player name</h1>
-      <p>Player picture</p>
-      <p>Player number</p>
-      <p>Player position</p>
+    <div className="flyersRoster">
+      <div className="flyers-filter">
+        <label>Filter by position</label>
+        <select value={position} onChange={handleFilter}>
+          <option value="All">All</option>
+          <option value="C">C</option>
+          <option value="LW">LW</option>
+          <option value="RW">RW</option>
+          <option value="D">D</option>
+          <option value="G">G</option>
+        </select>
+      </div>
+      {filteredPositions.map((flyer) => {
+        const {playerName, playerNumber, playerPosition} = flyer;
+        return (
+          <div className="flyersPlayer" key={playerNumber}>
+            <h1 className="playerName">{playerName}</h1>
+            <p className="roster-p">#{playerNumber}</p>
+            <p className="roster-p">{playerPosition}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/Rosters/Flyers.js
@@ -0,0 +1,164 @@
+const flyers = [
+  {
+    playerName: "Derick Brassard",
+    playerNumber: 19,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Patrick Brown",
+    playerNumber: 38,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Connor Bunnaman",
+    playerNumber: 82,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Sean Couturier",
+    playerNumber: 14,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Ryan Fitzgerald",
+    playerNumber: 51,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Morgan Frost",
+    playerNumber: 48,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Kevin Hayes",
+    playerNumber: 13,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Travis Konecny",
+    playerNumber: 11,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Scott Laughton",
+    playerNumber: 21,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Zack MacEwen",
+    playerNumber: 17,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Nate Thompson",
+    playerNumber: 44,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Maxwell William",
+    playerNumber: 71,
+    playerPosition: "C",
+  },
+  {
+    playerName: "Joel Farabee",
+    playerNumber: 86,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "Claude Giroux",
+    playerNumber: 28,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "Oskar Lindblom",
+    playerNumber: 23,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "Samuel Morin",
+    playerNumber: 55,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "Isaac Ratcliffe",
+    playerNumber: 76,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "James Van Riemsdyk",
+    playerNumber: 25,
+    playerPosition: "LW",
+  },
+  {
+    playerName: "Wade Allison",
+    playerNumber: 57,
+    playerPosition: "RW",
+  },
+  {
+    playerName: "Cam Atkinson",
+    playerNumber: 89,
+    playerPosition: "RW",
+  },
+  {
+    playerName: "Gerald Mayhew",
+    playerNumber: 27,
+    playerPosition: "RW",
+  },
+  {
+    playerName: "Justin Braun",
+    playerNumber: 61,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Kevin Connauton",
+    playerNumber: 8,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Ryan Ellis",
+    playerNumber: 94,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Ivan Provorov",
+    playerNumber: 9,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Rasmus Ristolainen",
+    playerNumber: 70,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Travis Sanheim",
+    playerNumber: 6,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Nick Seeler",
+    playerNumber: 24,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Keith Yandle",
+    playerNumber: 3,
+    playerPosition: "D",
+  },
+  {
+    playerName: "Carter Hart",
+    playerNumber: 79,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Martin Jones",
+    playerNumber: 35,
+    playerPosition: "G",
+  },
+  {
+    playerName: "Kirill Ustimenko",
+    playerNumber: 67,
+    playerPosition: "G",
+  },
+];
+
+export default flyers;
